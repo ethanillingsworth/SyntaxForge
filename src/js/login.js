641e81bbc51364d4b70c5f4b5a1d24e4f44c2def
Ethan Illingsworth <illingsworth.ethan@gmail.com>
2025-06-28 14:34:59 -0500

    fix login bug

--- src/js/login.js
+++ src/js/login.js
@@ -3,20 +3,19 @@ import { User } from "./main.js"
 import $ from "jquery"
 import { auth } from "./firebase.js"
 
-const email = $("#email")[0].value
-const password = $("#password").val()
-const username = $("#username").val()
+const email = $("#email")
+const password = $("#password")
+const username = $("#username")
 
 
 
 $("#signup").on("click", async () => {
-    User.create(email, password, username)
+    User.create(email.val(), password.val(), username.val())
     window.location.href = "/"
 })
 
 $("#login").on("click", () => {
-    console.log(email)
-    signInWithEmailAndPassword(auth, email, password)
+    signInWithEmailAndPassword(auth, email.val(), password.val())
         .then(() => {
             window.location.href = "/"
         })
