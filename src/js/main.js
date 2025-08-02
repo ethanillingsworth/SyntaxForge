13b177f222ecec87961666f655a5460bc6993309
Ethan Illingsworth <illingsworth.ethan@gmail.com>
2025-08-02 10:04:02 -0500

    Add docstrings to exported classes found in main, along with their functions Fixes #12

--- src/js/main.js
+++ src/js/main.js
@@ -20,12 +20,30 @@ const lessons = await (await fetch("/data/lessons.json")).json() || {}
 const courses = await (await fetch("/data/courses.json")).json() || {}
 
 
-
+/**
+* Creates a new user object
+*
+* @param {string} uid - User's unique id
+* @returns {User} The new User object
+*
+* @example
+* const user = new User("nouser")
+* const user = new User("ejnf24h24ufn2")
+*/
 export class User {
     constructor(uid) {
         this.uid = uid
     }
 
+    /**
+    * Get the users public data from firebase
+    *
+    * @returns {object} The data returned in the form of a dict
+    *
+    * @example
+    * const data = user.get() // returns {} if user.uid == "nouser" or data not found
+    * const data = user.get() // returns data if found
+    */
     async get() {
         if (this.uid == "nouser") {
             return {}
@@ -39,7 +57,14 @@ export class User {
             return {}
         }
     }
-
+    /**
+    * Check if a user is an admin or not
+    *
+    * @returns {boolean} True if admin, false if not
+    *
+    * @example
+    * const admin = user.admin() // true or false
+    */
     async admin() {
 
         if (this.uid == "nouser") {
@@ -55,14 +80,33 @@ export class User {
         return false
 
     }
-
+    /**
+    * Add data to users public directory
+    * 
+    * @param {object} data - Data to add users public dir
+    *
+    * @returns {null}
+    *
+    * @example
+    * user.update({"name": "test"}) // adds "name": "test" to public
+    */
     async update(data) {
         if (this.uid == "nouser") {
             return
         }
         await setDoc(doc(db, `public/${this.uid}`), data, { merge: true })
     }
-
+    /**
+    * Creates a new user
+    * 
+    * @param {string} email - Users email
+    * @param {string} password - Users password
+    *
+    * @returns {null}
+    *
+    * @example
+    * User.create("sample@syntaxforge.dev", "1234") // Creates user
+    */
     static async create(email, password) {
         createUserWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
@@ -86,23 +130,55 @@ export class User {
             });
     }
 }
-
+/**
+* Creates a new lesson object
+* 
+* @param {string} id - Lesson id (from lessons.json)
+*
+* @returns {Lesson} - Lesson object created
+*
+* @example
+* const lesson = new Lesson("zXtKRwB1rw4XMRXcE5Fn")
+*/
 export class Lesson {
     constructor(id) {
         this.id = id
     }
-
+    /**
+    * Returns Lesson data
+    *
+    * @returns {object} - Lesson data or {}
+    *
+    * @example
+    * const lessonData = lesson.get()
+    */
     async get() {
 
         return lessons[this.id] || {}
     }
 }
-
+/**
+* Creates a new course object
+* 
+* @param {string} id - Course id (from courses.json)
+*
+* @returns {Course} - Course object created
+*
+* @example
+* const course = new Course("js101")
+*/
 export class Course {
     constructor(id) {
         this.id = id
     }
-
+    /**
+    * Get all lessons with this course as a parent
+    *
+    * @returns {Lesson[]} - Sorted list of lessons by id in ascending order
+    *
+    * @example
+    * course.getLessons() // [Lesson1, Lesson2]
+    */
     async getLessons() {
         const filtered = Object.entries(lessons)
             .filter(([key, lesson]) => lesson.parent === this.id)
@@ -115,11 +191,27 @@ export class Course {
         const l = filtered.map(([key]) => new Lesson(key))
         return l
     }
-
+    /**
+    * Get all courses in courses.json
+    *
+    * @returns {Course[]} - List of courses
+    *
+    * @example
+    * Course.getAll() // [Course1, Course2]
+    */
     static async getAll() {
         return Object.keys(courses).map((id) => { return new Course(id) })
     }
-
+    /**
+    * Get a lesson from an index number
+    * 
+    * @param {string} num - Index number
+    *
+    * @returns {Lesson} - Lesson from course
+    *
+    * @example
+    * course.getLessonID(4) // Will get the lesson with index 4 under this course
+    */
     async getLessonID(num) {
         const match = Object.entries(lessons)
             .find(
@@ -128,11 +220,28 @@ export class Course {
 
         return match ? match[0] : null
     }
-
+    /**
+    * Returns Course data
+    *
+    * @returns {object} - Course data or {}
+    *
+    * @example
+    * const courseData = course.get()
+    */
     async get() {
         return courses[this.id] || {}
     }
-
+    /**
+    * Displays course info on /courses
+    * 
+    * @param {string} id - Id of the user to check for where to display courses
+    *
+    * @returns {object} - Course data or {}
+    *
+    * @example
+    * Course.display("SOMEUSERID")
+    * Course.display() // for a user logged out
+    */
     async display(id = "nouser") {
 
         const userData = await new User(id).get()
@@ -173,7 +282,19 @@ export class Course {
     }
 }
 
-
+/**
+* Creates a new editor object
+* 
+* @param {JQuery.<HTMLElement>} [parent = $("#editor")] - Element to place editor on
+* @param {string} [defaultCode = ""] - Element to place editor on
+*
+* @returns {Editor} - New editor object
+*
+* @example
+* const editor = new Editor()
+* const editor = new Editor($("#SOMEELEMENT")) // with parent
+* const editor = new Editor($("#SOMEELEMENT"), SOMECODEVAR) // with parent & default code
+*/
 export class Editor {
     constructor(parent = $("#editor"), defaultCode = "") {
         this.wrapper = $("<div>").addClass("editor row gap-0")
@@ -258,16 +379,35 @@ export class Editor {
         });
         
     }
-
+    /**
+    * Disables the editors terminal
+    * 
+    * @example
+    * editor.disableTerminal() // Terminal gets hidden
+    */
     disableTerminal() {
         this.terminal.addClass("hidden")
     }
-
+    /**
+    * Enables the editors terminal
+    * 
+    * @example
+    * editor.enableTerminal() // Terminal gets unhidden
+    */
     enableTerminal() {
         this.terminal.removeClass("hidden")
     }
 
-
+    /**
+    * Adds an element to the editors buttons
+    * 
+    * @param {JQuery.<HTMLElement>} e - Element to add
+    * @param {boolean} [back = false] - Place on the backside or frontside
+    *
+    * @example
+    * editor.addCustomButton($("#SOMEELMT"))
+    * editor.addCustomButton($("#SOMEELMT"), true) // adds to other side
+    */
     addCustomButton(e, back=false) {
         if (back) {
             this.buttonsBack.append(e)
@@ -276,7 +416,16 @@ export class Editor {
             this.buttons.append(e)
         }
     }
-
+    /**
+    * Safely evaluate users js on the frontend with a test case added if wanted
+    * 
+    * @param {string} input - Code to test
+    * @param {string} [test = null] - Test case or null for none
+    *
+    * @example
+    * editor.saveEval(editor.getContent()) // tests current content
+    * editor.saveEval(editor.getContent(), ";test == true") // tests current content with test case
+    */
     safeEval(input, test=null) {
 
         let logs = []
@@ -298,11 +447,25 @@ export class Editor {
         return { res: a(), logs: logs };
 
     }
-
+    /**
+    * Gets the editors current content
+    * 
+    * @returns {string} - Editors current content
+    * 
+    * @example
+    * editor.getContent() // "Some string"
+    */
     getContent() {
         return this.view.state.doc.toString();
     }
-
+    /**
+    * Sets editor content to a piece of code
+    * 
+    * @param {code} input - Code to set content to
+    *
+    * @example
+    * editor.setContent("// This is some codes")
+    */
     setContent(code) {
         this.view.dispatch({
             changes: {
