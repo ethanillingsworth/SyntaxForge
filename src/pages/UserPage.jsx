b3652421d939147bc8343323a2562ed53c387a4d
Ethan Illingsworth <illingsworth.ethan@gmail.com>
2026-06-04 17:49:59 -0500

    properly calculate xp now

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -5,89 +5,97 @@ import { useUser } from "../Global";
 import Rank from "../components/Rank";
 
 export default function UserPage() {
-	const { username } = useParams();
+    const { username } = useParams();
 
-	const [pageUser, setPageUser] = useState();
-	const [pageData, setPageData] = useState({});
-	const [rank, setRank] = useState({});
-	const [nextRank, setNextRank] = useState({});
+    const [pageUser, setPageUser] = useState();
+    const [pageData, setPageData] = useState({});
+    const [rank, setRank] = useState({});
+    const [nextRank, setNextRank] = useState({});
 
-	const user = useUser();
+    const [pageXp, setPageXp] = useState(0);
 
-	const [currentUserData, setUserData] = useState({});
+    const user = useUser();
 
-	useEffect(() => {
-		User.getFromUsername(username.split("@")[1]).then((u) => {
-			setPageUser(u);
-		});
-	}, [username]);
+    const [currentUserData, setUserData] = useState({});
 
-	useEffect(() => {
-		pageUser?.get().then((d) => {
-			setPageData(d);
-			setRank(pageUser.getCurrentRank(d.xp));
-			setNextRank(pageUser.getNextRank(d.xp));
-		});
-		user?.get().then((d) => {
-			setUserData(d);
-		});
-	}, [pageUser, user]);
+    useEffect(() => {
+        User.getFromUsername(username.split("@")[1]).then((u) => {
+            setPageUser(u);
+        });
+    }, [username]);
 
-	return (
-		<>
-			<div className="top flex flex-row mt-0 gap-4">
-				<img
-					src="https://placehold.co/150x150"
-					alt={username}
-					className="rounded overflow-hidden h-fit w-[150px]"
-				/>
+    useEffect(() => {
+        pageUser?.get().then((d) => {
+            setPageData(d);
+        });
 
-				<div className="flex flex-col gap-4">
-					<div className="flex flex-row gap-2 place-items-center">
-						<h1 className="normal-case m-0">
-							{pageData.displayName ? (
-								<>
-									{pageData.displayName}{" "}
-									<span className="text-zinc-400 text-2xl">
-										({username})
-									</span>
-								</>
-							) : (
-								username
-							)}
-						</h1>
-						<span className="px-2 p-1 gradient-bg-r text-black h-fit rounded">
-							Premium
-						</span>
-					</div>
+        pageUser?.getXp().then((xp) => {
+            setPageXp(xp);
 
-					<Rank
-						name={rank.name || "Loading..."}
-						color={rank.color || "#999999"}
-						xp={pageData.xp}
-						minXp={nextRank?.minXp}
-					></Rank>
+            setRank(pageUser.getCurrentRank(xp));
+            setNextRank(pageUser.getNextRank(xp));
+        });
 
-					<p className="text-zinc-400 text-lg">
-						{pageData.desc || "No Description"}
-					</p>
+        user?.get().then((d) => {
+            setUserData(d);
+        });
+    }, [pageUser, user]);
 
-					{currentUserData.username == pageData.username ? (
-						<button className="w-fit">Edit Profile</button>
-					) : null}
-				</div>
-			</div>
-			<h2>Courses</h2>
-			<div className="flex flex-row gap-2">
-				{Object.keys(pageData.courses || {}).map((id) => {
-					const c = pageData.courses[id];
-					if (c.added) {
-						return <CourseCard id={id} key={id} />;
-					}
-				})}
-			</div>
+    return (
+        <>
+            <div className="top flex flex-row mt-0 gap-4">
+                <img
+                    src="https://placehold.co/150x150"
+                    alt={username}
+                    className="rounded overflow-hidden h-fit w-[150px]"
+                />
 
-			<h2>Top Skills</h2>
-		</>
-	);
+                <div className="flex flex-col gap-4">
+                    <div className="flex flex-row gap-2 place-items-center">
+                        <h1 className="normal-case m-0">
+                            {pageData.displayName ? (
+                                <>
+                                    {pageData.displayName}{" "}
+                                    <span className="text-zinc-400 text-2xl">
+                                        ({username})
+                                    </span>
+                                </>
+                            ) : (
+                                username
+                            )}
+                        </h1>
+                        <span className="px-2 p-1 gradient-bg-r text-black h-fit rounded">
+                            Premium
+                        </span>
+                    </div>
+
+                    <Rank
+                        name={rank.name || "Loading..."}
+                        color={rank.color || "#999999"}
+                        xp={pageXp}
+                        minXp={nextRank?.minXp}
+                    ></Rank>
+
+                    <p className="text-zinc-400 text-lg">
+                        {pageData.desc || "No Description"}
+                    </p>
+
+                    {currentUserData.username == pageData.username ? (
+                        <button className="w-fit">Edit Profile</button>
+                    ) : null}
+                </div>
+            </div>
+            <h2>Courses</h2>
+            <div className="flex flex-row gap-2">
+                {/* {Object.keys(pageData.courses || {}).map((id) => {
+                    const c = pageData.courses[id];
+                    if (c.added) {
+                        return <CourseCard id={id} key={id} />;
+                    }
+                })} */}
+            </div>
+
+            <h2>Top Skills</h2>
+        </>
+    );
 }
